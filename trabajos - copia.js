31fe4ba4e7646beec821c1afbfacc55a1368ecb6
macarena piaggio <mpiaggio@dc.uba.ar>
2020-08-15 18:02:53 -0300

    funcionando con pocas cosas

--- trabajos - copia.js
+++
@@ -1,9 +0,0 @@
-var items = new vis.DataSet([
-{id: 1, content: 'item 1', start: '2014-04-20'},
-{id: 2, content: 'item 2', start: '2014-04-14'},
-{id: 3, content: 'item 3', start: '2014-04-18'},
-{id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19'},
-{id: 5, content:  createItem("item 5",'./img/swift.jpg',["https://makobot-prod.github.io/timelines/","blabla"])  , start: '2014-04-25'},
-{id: 6, content: 'item 6', start: '2014-04-27', type: 'point'},
-{id: "alpargatas_1", content: createItem("Alpargatas (1)","./img/alpargatas_1.png",["https://drive.google.com/open?id=1O9jsqaPcduQq4hcyB-j4U2A5_uPilS4d","https://drive.google.com/open?id=18Z6sNfTswZM45vVzMdFoQjTJyKX7pYkA","https://drive.google.com/open?id=1eAY7FMmt8iMdM3C4sSHRAx9Sq8pXkA4c","https://drive.google.com/open?id=1iV-vSbvNE8vCh3VgBXBoNs8mkNlQjAjr"]), start: "1883-07-01"}
-]);
