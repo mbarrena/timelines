69469cd2f2da0dd0bc36e75577184c1f18dca2f6
makobot-sh <piaggiomaca@gmail.com>
2021-03-13 16:55:43 -0300

    fixing 1st and 2nd unit

--- 1unidad/1unidad_appendable.js
+++ 1unidad/1unidad_appendable.js
@@ -1,13 +1,39 @@
 var items = new vis.DataSet([
-{id: "3500_anos_ac_-_aparicion_de_la_escr", content: "3500 años AC - Aparición de la escritura", image:"../img/informacion.svg", links:["http://drive.google.com/uc?export=view&id=1X6yVZSpVk7Kos92vo-PQlk4vl3TF7l0Y","http://drive.google.com/uc?export=view&id=1X6yVZSpVk7Kos92vo-PQlk4vl3TF7l0Y"], start: new Date(-3500,0,0)},
-{id: "1750_primera_revolucion_industrial.", content: "1750 Primera Revolución Industrial", image:"../img/informacion.svg", links:["http://drive.google.com/uc?export=view&id=1Bg8ql7-MegEuQ-8Rgx5YYY-XfVzryAW4","http://drive.google.com/uc?export=view&id=1Bg8ql7-MegEuQ-8Rgx5YYY-XfVzryAW4"], start: new Date(1750,0,0)},
-{id: "9000_anos_ac_la_revolucion_neolitic", content: "9000 años AC La Revolución Neolítica", image:"../img/informacion.svg", links:["http://drive.google.com/uc?export=view&id=1i6LJe9t9zw8GK9wnh15VWqjelRyZYTNy","http://drive.google.com/uc?export=view&id=1i6LJe9t9zw8GK9wnh15VWqjelRyZYTNy"], start: new Date(-9000,0,0)},
-{id: "1492_12_de_octubre_descubrimiento_d", content: "1492 12 de octubre Descubrimiento de América", image:"../img/informacion.svg", links:["http://drive.google.com/uc?export=view&id=1cCjReNnEiHTM4Gej3OgWvSDNLAfxP3UO","http://drive.google.com/uc?export=view&id=1cCjReNnEiHTM4Gej3OgWvSDNLAfxP3UO"], start: new Date(1492,10,12)},
-{id: "476_fin_imperio_romano_de_occidente", content: "476 Fin Imperio Romano de Occidente y fin Edad Antigua", image:"../img/informacion.svg", links:["http://drive.google.com/uc?export=view&id=1Dz6E1JZKvzrjPL0dSh9YI8o2Mjsnc2zk","http://drive.google.com/uc?export=view&id=1Dz6E1JZKvzrjPL0dSh9YI8o2Mjsnc2zk"], start: new Date(476,0,0)},
-{id: "476_caida_imperio_romano_de_occiden", content: "476 Caída Imperio Romano de Occidente Código QR", image:"../img/informacion.svg", links:["http://drive.google.com/uc?export=view&id=12M7SFOCaOJzd0GMqtI3U9Rx4YkQZshHh","http://drive.google.com/uc?export=view&id=12M7SFOCaOJzd0GMqtI3U9Rx4YkQZshHh"], start: new Date(476,0,0)},
-{id: "3001_ac_fines_-_comienza_la_escritu", content: "3001 aC (fines) - Comienza la Escritura", image:"../img/informacion.svg", link:"http://drive.google.com/uc?export=view&id=10h5MzE5TFNiPzvkIU2J_3kg_emD96WZs", start: new Date(-3001,0,0)},
-{id: "2340_ac_-_mesopotamia.pdf", content: "2340 aC - Mesopotamia", image:"../img/informacion.svg", link:"http://drive.google.com/uc?export=view&id=1572RSXAWOMc4oySfkGxCbnDwxZkvQL7m", start: new Date(-2340,0,0)},
-{id: "476_dc_caida_del_imperio_romano_de_", content: "476 DC Caída del Imperio Romano de Occidente", image:"../img/informacion.svg", link:"http://drive.google.com/uc?export=view&id=1Y5YElmPvuurnanYrVM_JHcUqTlIHa16d", start: new Date(476,0,0)},
-{id: "1780_a_1840_revolucion_industrial.p", content: "1780 a 1840 Revolución Industrial", image:"../img/informacion.svg", link:"http://drive.google.com/uc?export=view&id=1zbwGxYLCfJVXU8fah4PGcCobDP5NeNEN", start: new Date(1780,0,0)},
-{id: "476_caida_del_imperio_romano_de_occ", content: "476 Caída del Imperio Romano de Occidente (cont.)", image:"../img/informacion.svg", links:["http://drive.google.com/uc?export=view&id=1GlmBhgvP0uR8Ls1RFQn175M4O6vqIHg5","http://drive.google.com/uc?export=view&id=1PKtjlPHhpSDziXDpy09nOyvgPtfRgFdG"], start: new Date(476,0,0)},
+{id: "3000_ac_fines_iv_milenio_ac_aparici", content: "3000 aC (fines IV milenio aC) Aparición de la escritura", image:"../img/informacion.svg", links:[
+"https://drive.google.com/file/d/1FP18u5keXoVEEEhkTR1yDnMpLbjw54Gu/view?usp=sharing",
+"http://drive.google.com/uc?export=view&id=1X6yVZSpVk7Kos92vo-PQlk4vl3TF7l0Y","http://drive.google.com/uc?export=view&id=1X6yVZSpVk7Kos92vo-PQlk4vl3TF7l0Y",
+"https://drive.google.com/file/d/1hErBxmWE-l_KN0_zU1x5q4cR0DE39_j0/view?usp=sharing",
+"http://drive.google.com/uc?export=view&id=10h5MzE5TFNiPzvkIU2J_3kg_emD96WZs"
+], start: new Date(-3000,12,31)},
+{id: "9000_anos_ac_la_revolucion_neolitic", content: "9000 años AC La Revolución Neolítica", image:"../img/informacion.svg", links:[
+"http://drive.google.com/uc?export=view&id=1i6LJe9t9zw8GK9wnh15VWqjelRyZYTNy","http://drive.google.com/uc?export=view&id=1i6LJe9t9zw8GK9wnh15VWqjelRyZYTNy",
+"https://drive.google.com/file/d/1RnP1ORXvdTbsCBDmryAYdafjXOFPgi1y/view?usp=sharing"
+], start: new Date(-9000,0,0)},
+{id: "476_dc_caida_del_imperio_romano_de_", content: "476 DC Caída del Imperio Romano de Occidente", image:"../img/informacion.svg", links:[
+"http://drive.google.com/uc?export=view&id=1Y5YElmPvuurnanYrVM_JHcUqTlIHa16d",
+"http://drive.google.com/uc?export=view&id=1Dz6E1JZKvzrjPL0dSh9YI8o2Mjsnc2zk",
+"http://drive.google.com/uc?export=view&id=12M7SFOCaOJzd0GMqtI3U9Rx4YkQZshHh",
+"http://drive.google.com/uc?export=view&id=1GlmBhgvP0uR8Ls1RFQn175M4O6vqIHg5","http://drive.google.com/uc?export=view&id=1PKtjlPHhpSDziXDpy09nOyvgPtfRgFdG",
+"https://drive.google.com/file/d/1_4N2ZEyzv52eXxaX410R3hBV_-4tC-_7/view?usp=sharing","https://drive.google.com/file/d/1J6m3AwRKL6ZhOOKhc097eUq4cfcYGRCM/view?usp=sharing","https://drive.google.com/file/d/1pmEvpoVFieXq6LI5fo3aIA3_S6JA35qv/view?usp=sharing"
+], start: new Date(476,0,0)},
 {id: "1930_-_6_set_golpe_militar_de_uribu", content: "1930 - 6 set Golpe militar de Uriburu", image:"../img/informacion.svg", link:"http://drive.google.com/uc?export=view&id=1CGAZkNgDilpdEqC6rZ_kj15hTkPG2-kb", start: new Date(1930,9,6)},
+{id: "2340_ac:_mesopotamia", content: "2340 aC: Mesopotamia", image:"../img/informacion.svg", links:["https://drive.google.com/file/d/1hJ6lRO1KeLylrao2zEP6lOUo5ht-mV7j/view?usp=sharing",
+"http://drive.google.com/uc?export=view&id=1572RSXAWOMc4oySfkGxCbnDwxZkvQL7m"
+], start: new Date(-2340,06,01)},
+{id: "509_ac:_roma_se_convirtio_en_republ", content: "509 aC: Roma se convirtió en República ", image:"../img/informacion.svg", link:"https://drive.google.com/file/d/1XKCzCzR8mL2Hq53vGndhWYgUmM0pcI98/view?usp=sharing", start: new Date(-509,07,02)},
+{id: "siglos_xi,_xii_y_xiii:_feudalismo", content: "Siglos XI, XII y XIII: Feudalismo", image:"../img/informacion.svg", link:"https://drive.google.com/file/d/1xACO_EKKyg4wFIMV0qqTBvgqZoMU_HQD/view?usp=sharing", start: "1250-06-01"},
+{id: "1492_12_de_octubre_descubrimiento_d", content: "1492 12 de octubre Descubrimiento de América", image:"../img/informacion.svg", links:["http://drive.google.com/uc?export=view&id=1cCjReNnEiHTM4Gej3OgWvSDNLAfxP3UO","http://drive.google.com/uc?export=view&id=1cCjReNnEiHTM4Gej3OgWvSDNLAfxP3UO",
+"https://drive.google.com/file/d/1qt2i4LhWofTbzXIrau14fUu7Kod_SJWY/view?usp=sharing"
+], start: new Date(1492,10,12)},
+{id: "1-11-1700:_reformas_borbonicas_en_l", content: "1-11-1700: Reformas borbónicas en la Nueva España (fecha: muerte Carlos II)", image:"../img/informacion.svg", link:"https://drive.google.com/file/d/1NV_UuQ8H0SwSrBMK5nnPOGEt_n49UEdE/view?usp=sharing", start: "1700-11-01"},
+
+{id: "1780_a_1840:_revolucion_industrial", content: "1780 a 1840: Revolución Industrial", image:"../img/informacion.svg", links:["https://drive.google.com/file/d/1pQFDoTH5IjpgI61zjSFY_dvefD35N8jI/view?usp=sharing",
+"http://drive.google.com/uc?export=view&id=1zbwGxYLCfJVXU8fah4PGcCobDP5NeNEN",
+"http://drive.google.com/uc?export=view&id=1Bg8ql7-MegEuQ-8Rgx5YYY-XfVzryAW4","http://drive.google.com/uc?export=view&id=1Bg8ql7-MegEuQ-8Rgx5YYY-XfVzryAW4"
+], start: "1780-06-01"},
+{id: "1776:_fundacion_virreinato_rio_de_l", content: "1776: fundación Virreinato Río de la Plata", image:"../img/informacion.svg", link:"https://drive.google.com/file/d/19krFJVnwbOumLyev8l5ssNx-EtMlTW5W/view?usp=sharing", start: "1776-08-01"},
+{id: "05-05-1789:_revolucion_francesa", content: "05-05-1789: Revolución Francesa", image:"../img/informacion.svg", links:["https://drive.google.com/file/d/1VvgX8EZmstCelafJqLPFLoE3iZLiJktN/view?usp=sharing",
+"https://drive.google.com/file/d/1pIO9m0jnGgOO9upQBSXOld4XGnpx1pV9/view?usp=sharing"
+], start: "1789-05-05"},
+{id: "fines_sxviii_a_mediados_sxix:_roman", content: "Fines SXVIII a mediados SXIX: Romanticismo", image:"../img/informacion.svg", link:"https://drive.google.com/file/d/1lrfBs4IxPLEflcCE50LqkAiuRVGrsBim/view?usp=sharing", start: "1790-06-01"},
+{id: "1800:_segunda_revolucion_industrial", content: "1800: Segunda Revolución Industrial", start: "1800-07-01"},
